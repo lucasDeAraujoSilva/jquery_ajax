673948111649548a9ab0e4cfb4bdafc3223c00b8
lucasDeAraujoSilva <lucasaraujosilvamk@gmail.com>
2022-02-23 19:46:22 -0300

    aprendendo jquey e ajax com git e github

---
+++ main.js
@@ -0,0 +1,28 @@
+function consultaCep() {
+  $(".barra-progress").show();
+  var cep = document.getElementById("cep").value;
+  var url = "https://viacep.com.br/ws/" + cep + "/json/";
+  console.log(url);
+
+  $.ajax({
+    url: url,
+    type: "GET",
+    success: function (response) {
+      console.log(response);
+      $("#titulo-cep").html(" CEP " + cep);
+      document.getElementById("logradouro").innerHTML = response.logradouro;
+      document.getElementById("bairro").innerHTML = response.bairro;
+      $("#complemento").html(response.complemento);
+      $("#localidade").html(response.localidade);
+      document.getElementById("uf").innerHTML = response.uf;
+      $(".cep").show();
+      $(".barra-progress").hide();
+      //alert(response.cep);
+    },
+  });
+}
+
+$(function () {
+  $(".cep").hide();
+  $(".barra-progress").hide();
+});
